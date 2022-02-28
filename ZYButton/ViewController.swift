813ffd9249ce544d33dbcb9980a67d38269397f8
The-Sunshine <793894000@qq.com>
2022-02-28 21:08:49 +0800

    Udpate

--- ZYButton/ViewController.swift
+++ ZYButton/ViewController.swift
@@ -17,9 +17,8 @@ class ViewController: UIViewController {
         detailButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
         detailButton.setTitleColor(.red, for: .normal)
         self.view.addSubview(detailButton)
-        detailButton.setImageWithTitle(imageName: "mine_down", title: "title", type: .bottom, Space: 5)
+        detailButton.setImageWithTitle(imageName: "mine_down", title: "title", type: .top, Space: 5)
         detailButton.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
-        
         
 //        ZYButton.init(image: <#T##UIImage?#>, title: <#T##String?#>, type: <#T##ZYButtonImagePositionType?#>, imageSize: <#T##CGSize?#>, topOrLeftSpace: <#T##CGFloat?#>, imageLabelSpace: <#T##CGFloat?#>)
     }
